9322e48b0f2c243b5a1b41103d4a61a496c37d3f
joe5yellow <orange030@gmail.com>
2010-09-12 04:44:08 +0000

    Smooth Camera Update 2/2.

--- prototype/Assets/microcosmicWar/Scripts/_2DCameraFollow.js
+++ prototype/Assets/microcosmicWar/Scripts/_2DCameraFollow.js
@@ -22,16 +22,34 @@ function Update ()
 	{
 		if ( useSmooth)
 		{
-				if (Input.GetKey (KeyCode.A)) 
+				if (Input.GetButton ("left")) 
 				{
 					translationx = (target.position.x - 10.0 - transform.position.x) / positionDamping * Time.deltaTime;
+					if ( Input.GetButton ("up"))
+					{
+					translationy = (target.position.y + 8.0 - transform.position.y) / positionDamping * Time.deltaTime;
+					}
+					else if (Input.GetButton ("down"))
+					{
+					translationy = (target.position.y - 8.0 - transform.position.y) / positionDamping * Time.deltaTime;
+					}
+					else
 					translationy = (target.position.y - transform.position.y) / positionDamping * Time.deltaTime;
 					if ( transform.position.x != target.position.x && transform.position.y != target.position.y )
 					transform.Translate(translationx, translationy, 0);
 				}
-				else if ( Input.GetKey (KeyCode.D))
+				else if ( Input.GetButton ("right"))
 				{
 					translationx = (target.position.x + 10.0 - transform.position.x) / positionDamping * Time.deltaTime;
+					if ( Input.GetButton ("up"))
+					{
+					translationy = (target.position.y + 8.0 - transform.position.y) / positionDamping * Time.deltaTime;
+					}
+					else if (Input.GetButton ("down"))
+					{
+					translationy = (target.position.y - 8.0 - transform.position.y) / positionDamping * Time.deltaTime;
+					}
+					else
 					translationy = (target.position.y - transform.position.y) / positionDamping * Time.deltaTime;
 					if ( transform.position.x != target.position.x && transform.position.y != target.position.y )
 					transform.Translate(translationx, translationy, 0);
