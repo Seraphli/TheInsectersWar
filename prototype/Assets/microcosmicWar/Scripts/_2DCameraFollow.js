54371078463e05344ba06c32417f2c0f664f27d1
joe5yellow <orange030@gmail.com>
2010-09-12 04:17:38 +0000

    Smooth update 1/2

--- prototype/Assets/microcosmicWar/Scripts/_2DCameraFollow.js
+++ prototype/Assets/microcosmicWar/Scripts/_2DCameraFollow.js
@@ -3,11 +3,11 @@ var target:Transform;
 // Smooth switcher
 var useSmooth = true;
 // How much we 
-var positionDamping = 1.0;
+var positionDamping = 0.75;
 
 function Start()
 {
-	
+
 }
 
 function setTaget(pTarget:Transform)
@@ -22,10 +22,27 @@ function Update ()
 	{
 		if ( useSmooth)
 		{
+				if (Input.GetKey (KeyCode.A)) 
+				{
+					translationx = (target.position.x - 10.0 - transform.position.x) / positionDamping * Time.deltaTime;
+					translationy = (target.position.y - transform.position.y) / positionDamping * Time.deltaTime;
+					if ( transform.position.x != target.position.x && transform.position.y != target.position.y )
+					transform.Translate(translationx, translationy, 0);
+				}
+				else if ( Input.GetKey (KeyCode.D))
+				{
+					translationx = (target.position.x + 10.0 - transform.position.x) / positionDamping * Time.deltaTime;
+					translationy = (target.position.y - transform.position.y) / positionDamping * Time.deltaTime;
+					if ( transform.position.x != target.position.x && transform.position.y != target.position.y )
+					transform.Translate(translationx, translationy, 0);
+				}
+			else
+			{
 			translationx = (target.position.x - transform.position.x) / positionDamping * Time.deltaTime;
 			translationy = (target.position.y - transform.position.y) / positionDamping * Time.deltaTime;
 			if ( transform.position.x != target.position.x && transform.position.y != target.position.y )
-			transform.Translate = (translationx, translationy,o);
+			transform.Translate(translationx, translationy, 0);
+			}
 			//transform.position.y = Mathf.Lerp(transform.position.y, target.position.y, timeDamping);
 			//transform.position.x = Mathf.Lerp(transform.position.x, target.position.x, 1.75);
 			//transform.position.y = Mathf.Lerp(transform.position.y, target.position.y, 1.75); 
