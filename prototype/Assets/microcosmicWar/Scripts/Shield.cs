537fb0f98e9a0866f363fbe7a8bfc7104613fb04
orange030 <orange030@gmail.com>
2010-10-23 19:43:43 +0000

    防护盾 代码, 面向对象化 修改.

--- prototype/Assets/microcosmicWar/Scripts/Shield.cs
+++ prototype/Assets/microcosmicWar/Scripts/Shield.cs
@@ -1,7 +1,73 @@
 using UnityEngine;
 using System.Collections;
 
-public class Shield : MonoBehaviour {
+public class Shield : MonoBehaviour 
+{
+    [System.Serializable]
+    public class ShieldBasic
+    {
+
+        public float alpha = 0;
+        public float Timerbefore = 0;         //碰撞时的时间
+        public float countTimer;              //持续时间
+        public float changeSpeed;         //变化速度
+
+        public bool use = false;
+        private Transform shield;
+
+        public Transform shieldTransform
+        {
+            //get { return shield; }
+            set
+            {
+                shield = value;
+                applyColor();
+            }
+        }
+        public Color color;
+
+        //判断变化时间
+        public void Change()
+        {
+            ShieldBasic lShield = this;
+            if (lShield.use == true && (float)(Time.time - Timerbefore) < countTimer)
+            {
+                ShieldAppear();
+            }
+            else
+            {
+                lShield.use = false;
+                ShieldDispear();
+            }
+            applyColor();
+        }
+
+        //防护盾消失效果
+        private void ShieldDispear()
+        {
+            ShieldBasic changeShield = this;
+            if ((float)(Time.time - Timerbefore) > countTimer)
+            {
+                if (changeShield.alpha > 0)
+                    changeShield.alpha = changeShield.alpha - changeSpeed * Time.deltaTime;
+            }
+        }
+        //防护盾显示效果
+        private void ShieldAppear()
+        {
+            ShieldBasic changeShield = this;
+            if (changeShield.alpha < 1)
+            {
+                changeShield.alpha = changeShield.alpha + changeSpeed * Time.deltaTime;
+                Timerbefore = Time.time;
+            }
+        }
+
+        private void applyColor()
+        {
+            shield.renderer.material.color = new Color(1, 1, 1, alpha);
+        }
+    }
 
     public GameObject shield;
     public float countTimer = 10;          //持续时间
@@ -11,15 +77,27 @@ public class Shield : MonoBehaviour {
     private Vector3 vector;                 //到碰撞点的向量
     private ShieldBasic shieldLeft = new ShieldBasic();     //左面护甲
     private ShieldBasic shieldRight = new ShieldBasic();    //右面护甲
-    private float Timerbefore;                  //碰撞时的时间
-    private float Timerafter;                   //碰撞后的时间
-    private GameObject clone;                   //防护盾Prefab副本
+    //private float Timerbefore;                  //碰撞时的时间
+    //private float Timerafter;                   //碰撞后的时间
+    private GameObject shieldObject;                   //防护盾Prefab副本
 
 	// Use this for initialization
-	void Awake () {
+    //void Awake()
+    //{
+    //}
+
+    void Start()
+    {
+        shieldObject = gameObject;
         ShieldInstantiate();
-        SetColor();
-	}
+        //SetColor();
+
+        shieldLeft.countTimer = countTimer;
+        shieldRight.countTimer = countTimer;
+
+        shieldLeft.changeSpeed = changeSpeed;
+        shieldRight.changeSpeed = changeSpeed;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -32,68 +110,40 @@ public class Shield : MonoBehaviour {
         {
             vector = other.transform.position - transform.position;
             Destroy(other.gameObject);
-            Timerbefore = Time.time;
+            //Timerbefore = Time.time;
             if (vector.x >= 0)
             {
-                shieldLeft.use = true;
-            }
-            if (vector.x <= 0)
-            {
                 shieldRight.use = true;
+                shieldRight.Timerbefore = Time.time;
+            }
+            else if (vector.x <= 0)
+            {
+                shieldLeft.use = true;
+                shieldLeft.Timerbefore = Time.time;
             }
         }
     }
-    //判断变化时间
-    private void Change(ShieldBasic shield)
-        {
-            if (shield.use == true && (float)(Time.time - Timerbefore) < countTimer)
-            {
-                ShieldAppear(shield);
-            }
-            else
-            {
-                shield.use = false;
-                ShieldDispear(shield);
-            }
-        }
     //透明度变化
-	public void alphaChange () {
-        if (clone)
-        {
-            SetColor();
-            Change(shieldLeft);
-            Change(shieldRight);
-        }
-	}
+    public void alphaChange()
+    {
+        //if (shieldObject)
+        //{
+            //SetColor();
+            shieldLeft.Change();
+            shieldRight.Change();
+        //}
+    }
     //防护盾初始化
     private void ShieldInstantiate()
     {
-        clone = zzCreatorUtility.Instantiate(shield, transform.position, transform.rotation, 0);
-        shieldLeft.shield = clone.transform.Find("ShieldLeft");
-        shieldRight.shield = clone.transform.Find("ShieldRight");
+        //shieldObject = zzCreatorUtility.Instantiate(shield, transform.position, transform.rotation, 0);
+        shieldLeft.shieldTransform = shieldObject.transform.Find("ShieldLeft");
+        shieldRight.shieldTransform = shieldObject.transform.Find("ShieldRight");
     }
     //设置颜色,透明度
-    private void SetColor()
-    {
-        shieldLeft.shield.renderer.material.color = new Color(1, 1, 1, shieldLeft.alpha);
-        shieldRight.shield.renderer.material.color = new Color(1, 1, 1, shieldRight.alpha);
-    }
-    //防护盾消失效果
-    private void ShieldDispear(ShieldBasic changeShield)
-    {
-        if ((float)(Time.time - Timerbefore) > countTimer)
-        {
-            if (changeShield.alpha > 0)
-                changeShield.alpha = changeShield.alpha - changeSpeed * Time.deltaTime;
-        }
-    }
-    //防护盾显示效果
-    private void ShieldAppear(ShieldBasic changeShield)
-    {
-        if (changeShield.alpha < 1)
-        {
-            changeShield.alpha = changeShield.alpha + changeSpeed * Time.deltaTime;
-            Timerbefore = Time.time;
-        }
-    }
+    //private void SetColor()
+    //{
+    //    shieldLeft.shieldTransform.renderer.material.color = new Color(1, 1, 1, shieldLeft.alpha);
+    //    shieldRight.shieldTransform.renderer.material.color = new Color(1, 1, 1, shieldRight.alpha);
+    //}
 }
