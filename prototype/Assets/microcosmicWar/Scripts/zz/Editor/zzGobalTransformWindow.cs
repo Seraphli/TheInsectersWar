648fb81b3265cb3f0896f98892b6df20710b5ec3
orange030 <orange030@gmail.com>
2010-11-08 16:47:34 +0000

    add: 显示全局坐标的工具

---
+++ prototype/Assets/microcosmicWar/Scripts/zz/Editor/zzGobalTransformWindow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+//[CustomEditor(typeof(GameObject))]
+class zzGobalTransformWindow : EditorWindow
+{
+    [MenuItem("Window/Gobal Transform")]
+    static void Init()
+    {
+        EditorWindow.GetWindow(typeof(zzGobalTransformWindow),false,"全局变换");
+    }
+
+    //public override void OnInspectorGUI()
+    void OnGUI() 
+    {
+        //base.OnInspectorGUI();
+        Transform transform = Selection.activeTransform;
+        if (transform)
+        {
+            EditorGUILayout.BeginVertical();
+            EditorGUILayout.Vector3Field("全局位置", transform.position);
+            EditorGUILayout.Vector3Field("全局旋转", transform.rotation.eulerAngles);
+            EditorGUILayout.Vector3Field("全局放缩", transform.lossyScale);
+            EditorGUILayout.EndVertical();
+
+        }
+    }
+
+    void OnInspectorUpdate()
+    {
+        Repaint();
+    }
+}
