8b01c1a3e670cb51445e44b4d201cd52caaa0603
orange030 <orange030@gmail.com>
2010-12-02 11:40:22 +0000

    add:榴弹蚂蚁

---
+++ prototype/Assets/microcosmicWar/Scripts/BulletFaceVelocity.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+class BulletFaceVelocity : MonoBehaviour
+{
+    Bullet bullet;
+    //public Rigidbody bulletRigidbody;
+    void Start()
+    {
+        if (!bullet)
+            bullet = gameObject.GetComponent<Bullet>();
+    }
+
+    void Update()
+    {
+        bullet.setForward(bullet.bulletRigidbody.velocity);
+    }
+}
---
+++ prototype/Assets/microcosmicWar/Scripts/zz/zzRigidbodyUtility.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+class zzRigidbodyUtility:MonoBehaviour
+{
+    public Transform centerOfMass;
+    void Start()
+    {
+        var lRigidbody = gameObject.GetComponent<Rigidbody>();
+        lRigidbody.centerOfMass = transform.InverseTransformPoint(centerOfMass.position);
+    }
+}
