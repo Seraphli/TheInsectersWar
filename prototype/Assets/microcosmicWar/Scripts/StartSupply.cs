eae63a382fb321ed80dc988dec84b18d7117c17a
251875969 <orange030@gmail.com>
2010-12-03 13:58:36 +0000

    空投比例的修改  动画的增加

--- prototype/Assets/microcosmicWar/Scripts/StartSupply.cs
+++ prototype/Assets/microcosmicWar/Scripts/StartSupply.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class StartSupply : MonoBehaviour {
+public class StartSupply:MonoBehaviour{
 
 	// Use this for initialization
 	//void Start () {
@@ -10,92 +10,49 @@ public class StartSupply : MonoBehaviour {
 	
 	// Update is called once per frame
 
-	public GameObject planeLeft;
-	public GameObject planeRight;
-	public GameObject supplyBox;
-	private GameObject plane;
-	public float speed=10;
-	private bool supplyBool=false;
-	private bool boxBool=false;
-	private float randomX;
-	private float end;
+	//void Update () 
+	//{
 	
-	private float smoothF;
+	//}
+	public  GameObject plane;
+	private  GameObject planeGame;
 	
 	
-	void Update () 
+	
+	
+	
+	public  void startSupplyPlane(float velocity,float startX,float endX,float heightY)
 	{
-		if(supplyBool)
+		if(create(startX,heightY))
 		{
-			
-			plane.transform.position=new Vector3(plane.transform.position.x+speed*smoothF,plane.transform.position.y,plane.transform.position.z);
-			if((plane.transform.position.x-end)<5.0f&&(plane.transform.position.x-end)>-5.0f)
-			{
-				if(!boxBool)
-				{
-					Destroy(plane);	
-					supplyBool=false;
-				}
-			}
-			
-			
-			
-			
-			if(boxBool)
-			{
-				supplyBox.transform.position=new Vector3(supplyBox.transform.position.x+speed*smoothF,supplyBox.transform.position.y,supplyBox.transform.position.z);
-				if((supplyBox.transform.position.x-randomX)<1.0f&&(supplyBox.transform.position.x-randomX)>-1.0f)
-				{
-					supplyBox.GetComponent<Rigidbody>().isKinematic =false;
-					//supplyBox.GetComponent<Rigidbody>().AddForce(new Vector3(speed*smoothF*100000,0,0));
-					boxBool=false;
-				}
-					
-			}
+			SupplyData data;
+			data.velocity=velocity;
+			data.startX=startX;
+			data.endX=endX;
+			data.heightY=heightY;
+			planeGame.GetComponent<SupplyAirplane>().startPlane(data);
 		}
-	
+		
 	}
-	
-	
-	
-	
-	
-	
-	public void startSupplyPlane(float velocity,float startX,float endX,float hightY)
+	public  void startSupplyPlane(SupplyData data)
 	{
-		if(supplyBool!=true)
+		if(create(data.startX,data.heightY))
 		{
-			supplyBool=true;
-			boxBool=true;
-			
-		}
-		else
-		{
-				return ;
-		}
-		end=endX;
-		
-		
-		supplyBox=Instantiate (supplyBox,new Vector3(startX, hightY-1.0f, 0), Quaternion.identity) as GameObject;
-		supplyBox.GetComponent<Rigidbody>().isKinematic =true;
-		
-		randomX=Random.Range(startX,endX);
-		
-		if(startX<endX)
-		{
-				smoothF=Time.deltaTime;
-				
-				plane=Instantiate (planeRight,new Vector3(startX,hightY, 0), Quaternion.identity) as GameObject;
-		}
-		else
-		{
-				smoothF=Time.deltaTime*-1;
-				plane=Instantiate (planeLeft,new Vector3(startX,hightY, 0), Quaternion.identity) as GameObject;
-				
+			planeGame.GetComponent<SupplyAirplane>().startPlane(data);
 		}
 		
 	}
 	
+	
+	private  bool create(float x,float y)
+	{
+		if(planeGame==null)
+		{
+			planeGame=Instantiate (plane,new Vector3(x,y,0), Quaternion.identity) as GameObject;
+			return true;
+		}
+		return false;
+	}
 	
 	
 }
---
+++ prototype/Assets/microcosmicWar/Scripts/SupplyData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public struct SupplyData
+{
+		public float velocity;
+		public float startX;
+		public float endX;
+		public float heightY;
+}
