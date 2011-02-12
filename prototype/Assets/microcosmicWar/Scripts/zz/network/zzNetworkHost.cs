d4f66fe6633c8e51bf1eed2a778af957d107b9e0
orange030 <orange030@gmail.com>
2011-02-12 12:01:51 +0000

    none

--- prototype/Assets/microcosmicWar/Scripts/zz/network/zzNetworkHost.cs
+++ prototype/Assets/microcosmicWar/Scripts/zz/network/zzNetworkHost.cs
@@ -1,6 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class zzHostInfo
+{
+    public string gameType;
+    public string gameName;
+    public string comment;
+
+}
+
 public abstract class zzNetworkHost:MonoBehaviour
 {
     public abstract void RegisterHost(zzHostInfo pHostInfo);
