4a64fc37bae7ab35d65b425198f5e9db6d08fb60
orange030 <orange030@gmail.com>
2011-03-07 17:02:40 +0000

    add:蜜蜂轰炸机

---
+++ prototype/Assets/microcosmicWar/Scripts/Airplane.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Airplane:MonoBehaviour
+{
+    public ActionCommandControl actionCommandControl;
+    protected Transform reverseObjectTransform;
+    float Xscale;
+
+    public float speed=8.0f;
+    public Emitter emitter;
+
+    void Start()
+    {
+        reverseObjectTransform = transform.Find("reverse").transform;
+        Xscale = reverseObjectTransform.localScale.x;
+        UpdateFaceShow();
+    }
+
+    void UpdateFaceShow()
+    {
+        int lFace = actionCommandControl.getFaceValue();
+        Vector3 lTemp = reverseObjectTransform.localScale;
+        lTemp.x = lFace * Xscale;
+        reverseObjectTransform.localScale = lTemp;
+    }
+
+
+    void Update()
+    {
+        if (actionCommandControl.updateFace())
+        {
+            UpdateFaceShow();
+        }
+        transform.Translate(
+            speed * actionCommandControl.getFaceValue() * Time.deltaTime,
+            0f, 0f);
+
+        if (actionCommandControl.getCommand().Fire)
+        {
+            EmitBullet();
+        }
+    }
+
+    public void EmitBullet()
+    {
+        //if (life.isDead())
+        //{
+        //    return;
+        //}
+        var lbullet = emitter.EmitBullet()[0];
+        lbullet.rigidbody.velocity =
+            new Vector3(speed * actionCommandControl.getFaceValue(),0f,0f);
+    }
+}
---
+++ prototype/Assets/microcosmicWar/Scripts/System/ActionCommandInput.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class ActionCommandInput : MonoBehaviour
+{
+
+    public ActionCommandControl actionCommandControl;
+
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+
+    public KeyCode fire;
+    public KeyCode jump;
+
+    public void setToControl(ActionCommandControl pActionCommandControl)
+    {
+        actionCommandControl = pActionCommandControl;
+    }
+
+    public UnitActionCommand GetActionCommandFromInput()
+    {
+        UnitActionCommand lActionCommand = new UnitActionCommand();
+        if (Input.GetKey(left))
+        {
+            lActionCommand.FaceLeft = true;
+            lActionCommand.GoForward = true;
+        }
+        if (Input.GetKey(right))
+        {
+            lActionCommand.FaceRight = true;
+            lActionCommand.GoForward = true;
+        }
+        if (Input.GetKey(down))
+            lActionCommand.FaceDown = true;
+        if (Input.GetKey(up))
+            lActionCommand.FaceUp = true;
+
+        lActionCommand.Fire = Input.GetKeyDown(fire);
+        lActionCommand.Jump = Input.GetKeyDown(jump);
+        return lActionCommand;
+    }
+
+    void Update()
+    {
+        if (actionCommandControl)
+            actionCommandControl.setCommand(GetActionCommandFromInput());
+    }
+}
