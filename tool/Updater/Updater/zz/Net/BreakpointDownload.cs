7998027e7824442602611e467ed33b98ef6d2967
orange030 <orange030@gmail.com>
2011-08-28 07:17:45 +0000

    add:自动更新组件的下载部分

---
+++ tool/Updater/Updater/zz/Net/BreakpointDownload.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+
+namespace zz
+{
+    namespace Net
+    {
+        public class BreakpointDownload
+        {
+            public static HttpWebResponse getDownloadInfo(System.Uri pUri)
+            {
+                HttpWebRequest lRequest = (HttpWebRequest)HttpWebRequest.Create(pUri);
+                lRequest.Accept = "*/*";
+                return (HttpWebResponse)lRequest.GetResponse();
+            }
+
+            static WebHeaderCollection getHeader(System.Uri pUri)
+            {
+                var lHeader = new WebHeaderCollection();
+                //lHeader.Add("Host", pUri.Host);
+                //lHeader.Add("Get", pUri.PathAndQuery);
+                //lHeader.Add("Accept", "*/*");
+                lHeader.Add("Cache-Control", "no-cache");
+                //lHeader.Add("Connection", "Keep-Alive");
+                lHeader.Add("Pragma", "no-cache");
+                //lHeader.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)");
+                return lHeader;
+            }
+
+            public static bool download(System.Uri pUri, System.IO.Stream pWriterStream)
+            {
+                return download(pUri, 0, 0, pWriterStream);
+            }
+
+            public static bool download(System.Uri pUri, int pBegin, System.IO.Stream pWriterStream)
+            {
+                return download(pUri, pBegin, 0, pWriterStream);
+            }
+
+            public static bool download(System.Uri pUri, int pBegin, int pEnd, System.IO.Stream pWriterStream)
+            {
+                HttpWebRequest lRequest = (HttpWebRequest)HttpWebRequest.Create(pUri);
+                lRequest.Timeout = 30000;
+                lRequest.ReadWriteTimeout = 60000;
+                lRequest.Headers = getHeader(pUri);
+                if (pEnd > 0)
+                    lRequest.AddRange(pBegin, pEnd);
+                else
+                    lRequest.AddRange(pBegin);
+                lRequest.Accept = "*/*";
+                lRequest.KeepAlive = true;
+                lRequest.Method = WebRequestMethods.Http.Get;
+                //对发送的数据不使用缓存
+                lRequest.AllowWriteStreamBuffering = false;
+                var lResponse = (HttpWebResponse)lRequest.GetResponse();
+                using(var lResponseStream = lResponse.GetResponseStream())
+                {
+                    byte[] lBuffer = new byte[1024];
+                    int lSize = lResponseStream.Read(lBuffer, 0, lBuffer.Length);
+                    while (lSize > 0)
+                    {
+                        pWriterStream.Write(lBuffer, 0, lSize);
+                        lSize = lResponseStream.Read(lBuffer, 0, lBuffer.Length);
+                    }
+                }
+                lRequest.Abort();
+                lResponse.Close();
+                return true;
+
+            }
+        }
+    }
+}
---
+++ tool/Updater/Updater/zz/Net/Updater.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+
+namespace zz
+{
+    namespace Net
+    {
+        public class Updater
+        {
+            public string tempDir;
+            public string fileName;
+            public int bufferSize = 1000000;
+            public string[] downloadList
+            {
+                set
+                {
+                    downloadUriList = new System.Uri[value.Length];
+                    uriErrorCount = new int[value.Length];
+                    for(int i=0;i<value.Length;++i)
+                    {
+                        downloadUriList[i] = new System.Uri(value[i]);
+                    }
+                }
+            }
+            
+            int maxRetry = 3;
+            int errorCount = 0;
+            System.Uri[] downloadUriList;
+
+            //记录每个Uri的出错次数
+            int[] uriErrorCount;
+
+            void outError(string pInfo)
+            {
+                Console.WriteLine(pInfo);
+                Console.ReadLine();
+            }
+
+            public void download()
+            {
+                if (downloadUriList == null || downloadUriList.Length < 0)
+                    outError("更新失败:没有可用的更新地址");
+                while (errorCount < downloadUriList.Length * maxRetry)
+                {
+                    for (int i = 0; i < downloadUriList.Length; ++i)
+                    {
+                        if (uriErrorCount[i] >= maxRetry)
+                            continue;
+                        try
+                        {
+                            Console.WriteLine(downloadUriList[i]);
+                            if (download(downloadUriList[i]))
+                                return;
+                        }
+                        catch (IOException e)
+                        {
+                            Console.WriteLine(e);
+                            outError("更新失败:文件访问错误");
+                        }
+                        catch (System.Exception e)
+                        {
+                            Console.WriteLine(e);
+                            ++uriErrorCount[i];
+                            ++errorCount;
+                        }
+                    }
+                }
+                outError("更新失败");
+            }
+
+            public bool download(System.Uri lUri)
+            {
+                var lDownloadInfo = BreakpointDownload.getDownloadInfo(lUri);
+                lDownloadInfo.Close();
+
+                if ( string.IsNullOrEmpty(tempDir) && !Directory.Exists(tempDir))
+                {
+                    Directory.CreateDirectory(tempDir);
+                }
+
+                using (var lFile = new FileStream(tempDir + Path.DirectorySeparatorChar + fileName,
+                    FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite,bufferSize))
+                {
+                    if (lFile.Length != lDownloadInfo.ContentLength)
+                        continueDownload(lFile, lUri, lDownloadInfo.ContentLength);
+                    if (lFile.Length == lDownloadInfo.ContentLength)
+                        return true;
+                }
+                //BreakpointDownload.download(lUri, lMemoryStream);
+                //Console.Write(Encoding.ASCII.GetString(lMemoryStream.GetBuffer()));
+                return false;
+            }
+
+            public class DownloadInfoPrinter
+            {
+                public string info = string.Empty;
+                public float rate;
+                public float speed;
+                public float size;
+                public void refresh()
+                {
+                    var lCursorLeft = Console.CursorLeft;
+                    var lCursorTop = Console.CursorTop;
+                    Console.SetCursorPosition(0, Console.WindowHeight - 1);
+                    Console.Write("{0} {1:f2}m({2:f1}%) {3:f1}k/s          ",info, size, rate * 100f, speed);
+                    Console.CursorLeft = lCursorLeft;
+                    Console.CursorTop = lCursorTop;
+                }
+            }
+
+            class OutDownloadInfo : Stream
+            {
+                public DateTime lastPrintTime = DateTime.Now;
+                long _fileLength;
+                public long fileLength
+                {
+                    set
+                    {
+                        _fileLength = value;
+                        infoPrinter.size = (float)value / 1024f / 1024f;
+                    }
+                }
+                public string fileName
+                {
+                    set
+                    {
+                        infoPrinter.info = value;
+                    }
+                }
+                public int size = 0;
+                public Stream wrapedStream;
+                public DownloadInfoPrinter infoPrinter = new DownloadInfoPrinter();
+                public override bool CanRead { get { return wrapedStream.CanRead; } }
+                public override bool CanSeek { get { return wrapedStream.CanSeek; } }
+                public override bool CanWrite { get { return wrapedStream.CanWrite; } }
+                public override long Length { get { return wrapedStream.Length; } }
+                public override long Position
+                {
+                    get { return wrapedStream.Position; }
+                    set { wrapedStream.Position = value; }
+                }
+
+                public override void Flush() { wrapedStream.Flush(); }
+
+
+                public override int Read(byte[] buffer, int offset, int count)
+                {
+                    return wrapedStream.Read(buffer, offset, count);
+                }
+
+                public override long Seek(long offset, SeekOrigin origin)
+                {
+                    return wrapedStream.Seek(offset, origin);
+                }
+
+
+                public override void SetLength(long value)
+                {
+                    wrapedStream.SetLength(value);
+                }
+
+                public void refreshShow(TimeSpan pDeltaTime)
+                {
+                    infoPrinter.rate = (float)Length / (float)_fileLength;
+                    infoPrinter.speed = (float)size / 1024f / (float)pDeltaTime.TotalSeconds;
+                    infoPrinter.refresh();
+                }
+
+                public void refreshShow()
+                {
+                    infoPrinter.rate = (float)Length / (float)_fileLength;
+                    infoPrinter.speed = 0f;
+                    infoPrinter.refresh();
+                }
+
+                public override void Write(byte[] buffer, int offset, int count)
+                {
+                    var lNow = DateTime.Now;
+                    var lDelta = lNow - lastPrintTime;
+                    if (lDelta.Seconds >= 1)
+                    {
+                        refreshShow(lDelta);
+                        size = 0;
+                        lastPrintTime = lNow;
+                    }
+                    wrapedStream.Write(buffer, offset, count);
+                    size += count;
+                }
+            }
+
+            void continueDownload(FileStream pFileStream, System.Uri pUri, long pDataLength)
+            {
+                pFileStream.Position = pFileStream.Length;
+                using (var lOutDownloadInfo = new OutDownloadInfo()
+                {
+                    wrapedStream = pFileStream,
+                    fileLength = pDataLength,
+                    fileName = fileName,
+                })
+                {
+                    BreakpointDownload.download(pUri, (int)pFileStream.Length, lOutDownloadInfo);
+                    lOutDownloadInfo.refreshShow();
+                }
+
+            }
+        }
+    }
+}
