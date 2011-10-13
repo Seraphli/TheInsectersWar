a5ad4c0d04c65fb8f1c91c2b0c09cb49e3500bfd
orange030 <orange030@gmail.com>
2011-10-13 13:41:05 +0000

    none

---
+++ prototype/Assets/microcosmicWar/Scripts/Building/OperateObject/ShopFlowerControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ShopFlowerControl:MonoBehaviour
+{
+    public ShopBuilding shopBuilding;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("left"))
+            shopBuilding.backwardSelect();
+        if (Input.GetButtonDown("right"))
+            shopBuilding.forwardSelect();
+
+        if (Input.GetButtonDown("up"))
+            shopBuilding.buySeleted();
+        if (Input.GetButtonDown("down"))
+            shopBuilding.sellSeleted();
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            use(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            use(1);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            use(2);
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            use(3);
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            use(4);
+    }
+    void use(int pIndex)
+    {
+        shopBuilding.sell(pIndex);
+    }
+}
---
+++ prototype/Assets/microcosmicWar/Scripts/UI/ShopUI.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+
+public class ShopUI:MonoBehaviour
+{
+    [System.Serializable]
+    public class ItemUIControl
+    {
+        public ItemUIControl() { }
+        public ItemUIControl(zzInterfaceGUI pItem)
+        {
+            item = pItem;
+        }
+        [SerializeField]
+        zzInterfaceGUI _item;
+        public zzInterfaceGUI item
+        {
+            get { return _item; }
+            set
+            {
+                _item = value;
+                iconImage = value.getSubElement("icon");
+                countLabel = value.getSubElement("count");
+                changedCountLabel = value.getSubElement("changed");
+                priceLabel = value.getSubElement("price");
+            }
+        }
+
+        public bool visible
+        {
+            get { return _item.visible; }
+            set { _item.visible = value; }
+        }
+
+        //图标
+        public zzInterfaceGUI iconImage;
+        //买或卖价
+        public zzInterfaceGUI priceLabel;
+        //现在的数量
+        public zzInterfaceGUI countLabel;
+        //改变的数量
+        public zzInterfaceGUI changedCountLabel;
+
+        public Texture2D icon
+        {
+            set { iconImage.setImage(value); }
+        }
+
+        public int price
+        {
+            set { priceLabel.setText("$"+value.ToString()); }
+        }
+
+        public int count
+        {
+            set { countLabel.setText(value.ToString()); }
+        }
+
+        public int changedCount
+        {
+            set
+            {
+                if (value == 0)
+                    changedCountLabel.setText("");
+                else if (value > 0)
+                    changedCountLabel.setText("+" + value.ToString());
+                else
+                    changedCountLabel.setText(value.ToString());
+            }
+        }
+    }
+
+    public int flowerItemCount
+    {
+        get { return flowerItemUIList.Length; }
+    }
+
+    public ItemUIControl[] flowerItemUIControl;
+    public ItemUIControl[] equipmentItemUIControl;
+
+    public zzInterfaceGUI[] flowerItemUIList;
+    public zzInterfaceGUI[] equipmentUIList;
+
+    public zzInterfaceGUI costLabel;
+
+    public int cost
+    {
+        set
+        {
+            if (value > 0)
+                costLabel.setText("-$" + value);
+            else if (value < 0)
+                costLabel.setText("+$" + (-value));
+            else
+                costLabel.setText("$0");
+        }
+    }
+
+    public zzGUIAniToTargetAngle panToTargetAngle;
+
+    public float angularSpeed = 180f;
+
+    public void rotateToTarget(float pTarget)
+    {
+        panToTargetAngle.rotateToTarget(pTarget);
+    }
+
+    public float panAngle
+    {
+        get { return panToTargetAngle.angle; }
+        set { panToTargetAngle.angle = value; }
+    }
+
+    void Start()
+    {
+        flowerItemUIControl = new ItemUIControl[flowerItemUIList.Length];
+        for (int i = 0; i < flowerItemUIList.Length;++i )
+        {
+            flowerItemUIControl[i] =new ItemUIControl( flowerItemUIList[i] );
+        }
+
+        equipmentItemUIControl = new ItemUIControl[equipmentUIList.Length];
+        for (int i = 0; i < equipmentUIList.Length; ++i)
+        {
+            equipmentItemUIControl[i] = new ItemUIControl(equipmentUIList[i]);
+        }
+    }
+
+    public void clear()
+    {
+        foreach (var lControl in flowerItemUIControl)
+        {
+            lControl.visible = false;
+        }
+        foreach (var lControl in equipmentItemUIControl)
+        {
+            lControl.visible = false;
+        }
+    }
+
+}
